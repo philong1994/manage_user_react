73246a4decec430fc03f3d0b9c1c342b8465f1b7
Phi Long <philong@bin.com.vn>
2018-12-08 11:57:52 +0700

    add code

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class EditUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id : this.props.dataEditUser.id,
+            name : this.props.dataEditUser.name,
+            phone : this.props.dataEditUser.phone,
+            role : this.props.dataEditUser.role,
+        }
+    }
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name] : value
+        });       
+    }
     render() {
         return (
             <div className="row">
@@ -11,13 +27,13 @@ class EditUser extends Component {
                         </div>
                         <div className="card-body">
                             <div className="form-group">
-                                <input type="text" defaultValue={this.props.dataEditUser.name} className="form-control" name="name" id="name" placeholder="Tên"/>
+                                <input type="text" defaultValue={this.props.dataEditUser.name} onChange={(event) => this.isChange(event)} className="form-control" name="name" id="name" placeholder="Tên"/>
                             </div>
                             <div className="form-group">
-                                <input type="text" className="form-control" defaultValue={this.props.dataEditUser.phone}  name="phone" id="phone" placeholder="SĐT" />
+                                <input type="text" className="form-control" defaultValue={this.props.dataEditUser.phone} onChange={(event) => this.isChange(event)} name="phone" id="phone" placeholder="SĐT" />
                             </div>
                             <div className="form-group">
-                                <select name="role" id="role" defaultValue={this.props.dataEditUser.role}  className="form-control">
+                                <select name="role" id="role" defaultValue={this.props.dataEditUser.role} onChange={(event) => this.isChange(event)}  className="form-control">
                                 <option value="">Chọn quyền</option>
                                 <option value="user">User</option>
                                 <option value="admin">Admin</option>
--- src/components/InsertForm.js
+++ src/components/InsertForm.js
@@ -16,15 +16,7 @@ class InsertForm extends Component {
         const value = event.target.value;
         this.setState({
             [name] : value
-        });
-        
-        // var item = {};
-        // item.id = "";
-        // item.name = this.state.txtName;
-        // item.phone = this.state.txtPhone;
-        // item.role = this.state.sltRole;
-        
-        
+        });       
     }
     showForm = () => {
         if(this.props.buttonStatus === true){
@@ -42,7 +34,7 @@ class InsertForm extends Component {
                                 <input type="text" className="form-control" name="phone" id="phone" placeholder="SĐT"  onChange = {(event) => this.isChange(event)} />
                             </div>
                             <div className="form-group">
-                                <select name="role" id="role" className="form-control" onChange={(event) => this.isChange(event)}>
+                                <select name="role" id="role" className="form-control" >
                                 <option value="">Chọn quyền</option>
                                 <option value="User">User</option>
                                 <option value="Admin">Admin</option>
